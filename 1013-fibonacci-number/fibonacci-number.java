b4d30e554b856ea1d29d1acbd3e6241f77bc253e
Meenakshi <m54166078@gmail.com>
2026-09-19 12:02:57 +0530

    Time: 8 ms (43.59%) | Memory: 42.1 MB (52.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,26 @@
 import java.util.*;
 class Solution {
     public int fib(int n) {
-        Scanner sc=new Scanner(System.in);
+        // Scanner sc=new Scanner(System.in);
         
-        int a=0;
-        int b=1;
-        for(int i=0;i<n;i++){
-            System.out.println(a+" ");
+        // int a=0;
+        // int b=1;
+        // for(int i=0;i<n;i++){
+        //     System.out.println(a+" ");
 
-            int c=a+b;
-            a=b;
-            b=c;
-        }
-        return a;
+        //     int c=a+b;
+        //     a=b;
+        //     b=c;
+        // }
+        // return a;
+        return fibo(n);
     }        
+    public static int fibo(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
 }
